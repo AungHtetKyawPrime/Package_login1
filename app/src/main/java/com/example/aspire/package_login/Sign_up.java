34c4e340cd06a715c904a7338902685c76d772fa
Aung Htet Kyaw <whofluni@gmail.com>
2019-01-13 13:29:27 +0630

    packages_page added

--- app/src/main/java/com/example/aspire/package_login/Sign_up.java
+++ app/src/main/java/com/example/aspire/package_login/Sign_up.java
@@ -1,7 +1,10 @@
 package com.example.aspire.package_login;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Sign_up extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class Sign_up extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        TextView textView=(TextView)findViewById(R.id.login);
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(Sign_up.this,Packages.class));
+            }
+        });
+
+
     }
 }
